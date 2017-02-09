9e9a950cc88dcda0567adb85f6216597c24c90ec
MikeB <burton.mj@gmail.com>
2017-02-08 17:02:22 -0700

    mid progress

--- ctrl.js
+++ ctrl.js
@@ -1,3 +1,23 @@
 angular.module('appName').controller('mainCtrl', function($scope){
-  $scope.test = "mike burton"
+
+  $scope.niceList = [];
+  $scope.naughtyList = [];
+
+  $scope.addNewPerson = function(){
+    var differenceScore = $scope.niceScore - $scope.naughtyScore;
+
+    if (differenceScore >= 10){
+      $scope.niceList.push(buildPerson(differenceScore));
+    } else {
+        $scope.naughtyList.push(buildPerson(differenceScore));
+    }
+  };
+
+function buildPerson(differenceScore){
+  return {
+    name: $scope.name,
+    score: differenceScore
+  }
+}
+
 })
